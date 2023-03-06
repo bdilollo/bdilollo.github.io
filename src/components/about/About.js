2685ef5897f5435e217ddb5f352b7d9fcf67657d
bdilollo <113383715+bdilollo@users.noreply.github.com>
2023-03-06 14:51:02 -0600

    added About section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import ME from '../../assets/about-me.jpg';
+import { FaAward } from 'react-icons/fa';
+import { VscFolderLibrary } from 'react-icons/vsc';
+import './about.css';
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h2>About Me</h2>
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+            <img src={ME} alt='about image' />
+          </div>
+        </div>
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FaAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>1 year</small>
+            </article>
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon' />
+              <h5>Projects</h5>
+              <small>20+, Front- & Back-End</small>
+            </article>
+          </div>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a href='#contact' className='btn btn-primary'>Contact Me</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
