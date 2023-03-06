7f2dc3f0332c3b1497a1bbe0c109abe6dd86e53b
bdilollo <113383715+bdilollo@users.noreply.github.com>
2023-03-06 16:53:20 -0600

    added portfolio section

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,8 +1,76 @@
-import React from 'react'
+import React from 'react';
+import IMG from '../../assets/example-portfolio-item.jpg';
+import './portfolio.css';
+
+const data = [
+  {
+    id: 1,
+    image: IMG,
+    title: 'Example Portfolio Item 1',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 2,
+    image: IMG,
+    title: 'Example Portfolio Item 2',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 3,
+    image: IMG,
+    title: 'Example Portfolio Item 3',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 4,
+    image: IMG,
+    title: 'Example Portfolio Item 4',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 5,
+    image: IMG,
+    title: 'Example Portfolio Item 5',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 6,
+    image: IMG,
+    title: 'Example Portfolio Item 6',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio_container'>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio_item'>
+                <div className='portfolio_item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio_item-cta'>
+                  <a href={github} className='btn' target='_blank' rel='noreferrer'>GitHub</a>
+                  <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
