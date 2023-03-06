54dde0f1f248909519ac0b11b68ea828195eb05a
bdilollo <113383715+bdilollo@users.noreply.github.com>
2023-03-06 12:29:56 -0600

    created header, including social links and style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import CV from '../../assets/Bradley-DiLollo-CV.pdf';
+import ME from '../../assets/me.png';
+import HeaderLinks from './HeaderLinks';
+import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header_container'>
+        <h1>Bradley DiLollo</h1>
+        <h5 className='text-light'>Software Developer</h5>
+        <div className='cta'>
+          <a href={CV} download className='btn'>Download CV</a>
+          <a href='#contact' className='btn btn-primary'>Contact Me</a>
+        </div>
+        <HeaderLinks />
+        <div className='me'>
+          <img src={ME} alt='me' />
+        </div>
+          
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderLinks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+
+const HeaderLinks = () => {
+  return (
+    <div className='header_links'>
+        <a href='https://www.linkedin.com/in/bradley-dilollo/' target='_blank' rel='noreferrer'><BsLinkedin /></a>
+        <a href='https://github.com/bdilollo' target='_blank' rel='noreferrer'><BsGithub /></a>
+    </div>
+  )
+}
+
+export default HeaderLinks
