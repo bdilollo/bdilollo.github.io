b14b5a19913fbb790d4c8ee9f3099603ab069921
bdilollo <113383715+bdilollo@users.noreply.github.com>
2023-03-06 13:58:34 -0600

    added nav component

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './nav.css';
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+} from 'react-icons/ai';
+import {
+  BiBook,
+  BiMessageDetail
+} from 'react-icons/bi';
+import { useState } from 'react';
 
 const Nav = () => {
+
+const [activeNav, setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}>
+          <AiOutlineHome />
+      </a>
+      <a href='#about' 
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}>
+          <AiOutlineUser />
+      </a>
+      <a href='#experience'
+        onClick={() => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}>
+          <BiBook />
+      </a>
+      <a href='#contact'
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}>
+          <BiMessageDetail />
+      </a>
+    </nav>
   )
 }
 
