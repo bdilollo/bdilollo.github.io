40831d2d5622d17866d4611f6aa87c824bb4ddec
bdilollo <113383715+bdilollo@users.noreply.github.com>
2023-03-09 12:30:54 -0600

    added projects to portfolio section

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,49 +1,55 @@
 import React from 'react';
 import IMG from '../../assets/example-portfolio-item.jpg';
+import port1 from '../../assets/port1.png';
+import port2 from '../../assets/port2.png';
+import port3 from '../../assets/port3.png';
+import port4 from '../../assets/port4.png';
+import port5 from '../../assets/port5.png';
+import port6 from '../../assets/port6.png';
 import './portfolio.css';
 
 const data = [
   {
     id: 1,
-    image: IMG,
-    title: 'Example Portfolio Item 1',
-    github: 'https://github.com',
-    demo: 'https://github.com'
+    image: port1,
+    title: 'City Grader',
+    github: 'https://github.com/bdilollo/neighborhood-grader',
+    demo: 'https://joedjensen.github.io/neighborhood-grader/'
   },
   {
     id: 2,
-    image: IMG,
-    title: 'Example Portfolio Item 2',
-    github: 'https://github.com',
-    demo: 'https://github.com'
+    image: port2,
+    title: 'Yoked',
+    github: 'https://github.com/bdilollo/Yoked',
+    demo: 'https://young-headland-04692.herokuapp.com/login'
   },
   {
     id: 3,
-    image: IMG,
-    title: 'Example Portfolio Item 3',
-    github: 'https://github.com',
-    demo: 'https://github.com'
+    image: port3,
+    title: 'Google Books Search Engine',
+    github: 'https://github.com/bdilollo/book-search-engine',
+    demo: 'https://ancient-fortress-13590.herokuapp.com/'
   },
   {
     id: 4,
-    image: IMG,
-    title: 'Example Portfolio Item 4',
-    github: 'https://github.com',
-    demo: 'https://github.com'
+    image: port4,
+    title: 'PWA Text Editor',
+    github: 'https://github.com/bdilollo/text-editor',
+    demo: 'https://ancient-sea-72366.herokuapp.com/'
   },
   {
     id: 5,
-    image: IMG,
-    title: 'Example Portfolio Item 5',
-    github: 'https://github.com',
-    demo: 'https://github.com'
+    image: port5,
+    title: 'README Generator',
+    github: 'https://github.com/bdilollo/README-generator',
+    demo: 'https://drive.google.com/file/d/1r95JEXFdsQ1D4x7uZxhFWbNeUCWYBO4O/view'
   },
   {
     id: 6,
-    image: IMG,
-    title: 'Example Portfolio Item 6',
-    github: 'https://github.com',
-    demo: 'https://github.com'
+    image: port6,
+    title: 'E-Commerce Back End',
+    github: 'https://github.com/bdilollo/e-commerce-back-end',
+    demo: 'https://drive.google.com/file/d/1Z-ZdZURAf6cpGZ2t1hiUl0zOkVd6JGHK/view'
   },
 ]
 
