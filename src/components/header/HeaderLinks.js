ff0b0765abc127230e1cb2ab600d6fcaa607c29e
bdilollo <113383715+bdilollo@users.noreply.github.com>
2023-03-10 10:00:13 -0600

    minor tweak to github link

--- src/components/header/HeaderLinks.js
+++ src/components/header/HeaderLinks.js
@@ -5,7 +5,7 @@ const HeaderLinks = () => {
   return (
     <div className='header_links'>
         <a href='https://www.linkedin.com/in/bradley-dilollo/' target='_blank' rel='noreferrer'><BsLinkedin /></a>
-        <a href='https://github.com/bdilollo' target='_blank' rel='noreferrer'><BsGithub /></a>
+        <a href='https://github.com/bdilollo?tab=repositories' target='_blank' rel='noreferrer'><BsGithub /></a>
     </div>
   )
 }
