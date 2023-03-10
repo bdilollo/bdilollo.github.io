fc48f17e392d46a8c137e5b5efdf35e2de11d7c6
bdilollo <113383715+bdilollo@users.noreply.github.com>
2023-03-10 09:59:19 -0600

    added map method to reduce code

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -3,6 +3,58 @@ import { BsFillPatchCheckFill } from 'react-icons/bs';
 import './experience.css';
 
 const Experience = () => {
+  const FEdata = [
+    {
+      skill: 'HTML',
+      exp: 'Experienced'
+    },
+    {
+      skill: 'CSS',
+      exp: 'Intermediate'
+    },
+    {
+      skill: 'JavaScript',
+      exp: 'Experienced'
+    },
+    {
+      skill: 'React',
+      exp: 'Experienced'
+    },
+    {
+      skill: 'Material UI',
+      exp: 'Intermediate'
+    },
+    {
+      skill: 'Responsive Web Design',
+      exp: 'Intermediate'
+    }
+  ];
+  const BEdata = [
+    {
+      skill: 'Node.js',
+      exp: 'Experienced'
+    },
+    {
+      skill: 'Express.js',
+      exp: 'Experienced'
+    },
+    {
+      skill: 'MySQL',
+      exp: 'Intermediate'
+    },
+    {
+      skill: 'MongoDB',
+      exp: 'Intermediate'
+    },
+    {
+      skill: 'REST APIs',
+      exp: 'Experienced'
+    },
+    {
+      skill: 'GraphQL',
+      exp: 'Beginner'
+    }
+  ];
   return (
     <section id='experience'>
       <h5>Skills</h5>
@@ -11,82 +63,38 @@ const Experience = () => {
         <div className='experience_frontend'>
           <h3>Front-End Development</h3>
           <div className='experience_content'>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon'/>
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>CSS</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>JavaScript</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>React</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>Material UI</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
+            {
+              FEdata.map(({ skill, exp }) => {
+                return (
+                  <article className='experience_details'>
+                    <BsFillPatchCheckFill className='experience_details-icon'/>
+                    <div>
+                      <h4>{skill}</h4>
+                      <small className='text-light'>{exp}</small>
+                    </div>
+                  </article>
+                )
+              })
+            }
           </div>
         </div>
 
         <div className='experience_backend'>
         <h3>Back-End Development</h3>
           <div className='experience_content'>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>Node.js</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>MySQL</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>MongoDB</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>REST APIs</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>GraphQL</h4>
-                <small className='text-light'>Beginner</small>
-              </div>
-            </article>
+            {
+              BEdata.map(({ skill, exp }) => {
+                return(
+                  <article className='experience_details'>
+                    <BsFillPatchCheckFill className='experience_details-icon' />
+                    <div>
+                      <h4>{skill}</h4>
+                      <small className='text-light'>{exp}</small>
+                    </div>
+                  </article>
+                )
+              })
+            }
           </div>
         </div>
       </div>
